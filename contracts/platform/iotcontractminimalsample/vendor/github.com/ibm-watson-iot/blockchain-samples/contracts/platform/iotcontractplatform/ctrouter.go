e4be827c1f034c68b4fb0bd5fd993a591e66ada3
Nihal Ansari <nihalansari@googlemail.com>
2017-08-16 01:36:09 +0530

    certificate sub structure
    DMACert struct

--- contracts/platform/iotcontractminimalsample/vendor/github.com/ibm-watson-iot/blockchain-samples/contracts/platform/iotcontractplatform/ctrouter.go
+++ contracts/platform/iotcontractminimalsample/vendor/github.com/ibm-watson-iot/blockchain-samples/contracts/platform/iotcontractplatform/ctrouter.go
@@ -72,7 +72,7 @@ type ResponseStruct struct {
 							GrAf				string	`json:"grAf"`
 							DmaMasdat			string	`json:"dmaMasdat"`
 							AfDmaTest			string	`json:"afDmaTest"`
-							DmaCert			{ Cert string	`json:"cert"` }
+							DmaCert		struct	{ Cert string	`json:"cert"` }
 							AfDoc				string	`json:"afDoc"`
 							Caller				string  `json:"caller"`		//the UI/person who fired the transaction
 							V5cid           string `json:"v5cID"`
