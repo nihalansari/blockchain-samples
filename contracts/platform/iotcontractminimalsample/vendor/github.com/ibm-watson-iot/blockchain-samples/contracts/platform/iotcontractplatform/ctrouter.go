a9f36dc7f42ac71b06ea8f89d2d8d85074c1c092
Nihal Ansari <nihalansari@googlemail.com>
2017-08-16 08:33:29 +0530

    test responseStruct
    with empty struct

--- contracts/platform/iotcontractminimalsample/vendor/github.com/ibm-watson-iot/blockchain-samples/contracts/platform/iotcontractplatform/ctrouter.go
+++ contracts/platform/iotcontractminimalsample/vendor/github.com/ibm-watson-iot/blockchain-samples/contracts/platform/iotcontractplatform/ctrouter.go
@@ -40,35 +40,9 @@ type SimpleChaincode struct{}
 type ChaincodeFunc func(stub shim.ChaincodeStubInterface, args []string) ([]byte, error)
 
 
-type tempAsset struct {
-
-							
-							TransactionType 	string	`json:"transactionType"`
-							OwnerId				string	`json:"ownerId"`
-							AssetId				string	`json:"assetID"`		//BLOCKCHAIN KEY
-																				//NOTE: assetId changed to assetID 
-							MatnrAf				string	`json:"matnrAf"`
-							PoDma				string	`json:"poDma"`
-							PoSupp				string	`json:"poSupp"`
-							DmaDelDate			string	`json:"dmaDelDate"`
-							AfDelDate			string	`json:"afDelDate"`
-							TruckMod			string	`json:"truckMod"`
-							TruckPDate			string	`json:"truckPdate"`
-							TruckChnum			string	`json:"truckChnum"`
-							TruckEnnum			string	`json:"truckEnnum"`
-							SuppTest			string	`json:"suppTest"`
-							GrDma				string	`json:"grDma"`
-							GrAf				string	`json:"grAf"`
-							DmaMasdat			string	`json:"dmaMasdat"`
-							AfDmaTest			string	`json:"afDmaTest"`
-							DmaCert		struct	{ Cert string	`json:"cert"` }
-							AfDoc				string	`json:"afDoc"`
-							Caller				string  `json:"caller"`		//the UI/person who fired the transaction
-							
-			}
 
 // Structure to parse response JSON BEGIN
-type ResponseStruct struct {
+/*type ResponseStruct struct {
 
 		Assetclass struct {
 				name 	string	`json:"name"`
@@ -121,6 +95,53 @@ type ResponseStruct struct {
 		}
 
 }
+*/
+
+
+type ResponseStruct struct {
+
+		Assetclass struct {
+				name 	string	`json:"name"`
+				prefix 	string	`json:"prefix"`
+				assetIDpath 	string	`json:"assetIDpath"`	
+		}
+
+		AssetKey string `json:"assetkey"`
+
+		AssetState struct {
+							Asset struct {	
+							
+							TransactionType 	string	`json:"transactionType"`
+							OwnerId				string	`json:"ownerId"`
+							AssetId				string	`json:"assetID"`		//BLOCKCHAIN KEY
+																				//NOTE: assetId changed to assetID 
+							MatnrAf				string	`json:"matnrAf"`
+							PoDma				string	`json:"poDma"`
+							PoSupp				string	`json:"poSupp"`
+							DmaDelDate			string	`json:"dmaDelDate"`
+							AfDelDate			string	`json:"afDelDate"`
+							TruckMod			string	`json:"truckMod"`
+							TruckPDate			string	`json:"truckPdate"`
+							TruckChnum			string	`json:"truckChnum"`
+							TruckEnnum			string	`json:"truckEnnum"`
+							SuppTest			string	`json:"suppTest"`
+							GrDma				string	`json:"grDma"`
+							GrAf				string	`json:"grAf"`
+							DmaMasdat			string	`json:"dmaMasdat"`
+							AfDmaTest			string	`json:"afDmaTest"`
+							DmaCert		struct	{ Cert string	`json:"cert"` }
+							AfDoc				string	`json:"afDoc"`
+							Caller				string  `json:"caller"`		//the UI/person who fired the transaction
+							
+										} 
+							}
+		EventPayload struct {
+
+		}
+		EventOut struct		{
+		}
+
+}
 
 //END
 
@@ -152,6 +173,35 @@ type InRequest struct {
 		
 		} 
 }
+
+
+type tempAsset struct {
+
+							
+							TransactionType 	string	`json:"transactionType"`
+							OwnerId				string	`json:"ownerId"`
+							AssetId				string	`json:"assetID"`		//BLOCKCHAIN KEY
+																				//NOTE: assetId changed to assetID 
+							MatnrAf				string	`json:"matnrAf"`
+							PoDma				string	`json:"poDma"`
+							PoSupp				string	`json:"poSupp"`
+							DmaDelDate			string	`json:"dmaDelDate"`
+							AfDelDate			string	`json:"afDelDate"`
+							TruckMod			string	`json:"truckMod"`
+							TruckPDate			string	`json:"truckPdate"`
+							TruckChnum			string	`json:"truckChnum"`
+							TruckEnnum			string	`json:"truckEnnum"`
+							SuppTest			string	`json:"suppTest"`
+							GrDma				string	`json:"grDma"`
+							GrAf				string	`json:"grAf"`
+							DmaMasdat			string	`json:"dmaMasdat"`
+							AfDmaTest			string	`json:"afDmaTest"`
+							DmaCert		struct	{ Cert string	`json:"cert"` }
+							AfDoc				string	`json:"afDoc"`
+							Caller				string  `json:"caller"`		//the UI/person who fired the transaction
+							
+			}
+
 
 var router = make(map[string]ChaincodeRoute, 0)
 
